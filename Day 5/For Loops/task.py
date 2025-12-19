0b3d60beb2b8c63d86e62bad1f7ed5cfd4c62a3f
AbdulRehmanSaeed <140309861+AbdulRehman393@users.noreply.github.com>
2025-12-19 14:38:47 +0500

    Delete Day 5 directory

--- Day 5/For Loops/task.py
+++
@@ -1,9 +0,0 @@
-#Loops allow us to tell the computer to repeat actions without
-# having to write repeated code.
-# For Loops
-fruits = ["Apple", "Peach", "Pear"]
-for fruit in fruits:
-    print(fruit)
-    print(fruit+ "pie")
-    print(fruits)
-print(fruits)
