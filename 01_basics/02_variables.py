5bdd285f52ef514ec60662b50d7f285690b70e44
AbdulRehmanSaeed <140309861+AbdulRehman393@users.noreply.github.com>
2025-12-24 04:25:49 +0500

    Add files via upload

---
+++ 01_basics/02_variables.py
@@ -0,0 +1,54 @@
+# Variable = A container for a value ( string, integer , float , and boolean)
+#            A variable behaves as if it was the value it contains.
+
+
+first_name = "Ali"
+food = "Burger"
+email ="ali8@example.com"
+
+# Strings = A string is a series of characters , these include number, but we treat them as characters
+print(f"Hello {first_name}")   # This is f-string , here f means format, you place your variable inside placeholder {}
+print(f"You like {food}")
+print(f"Your email is: {email}")
+
+# Integers = Whole numbers
+age = 22
+quantity = 4
+num_of_students = 35
+
+print(f"You are {age} years old")
+print(f"You are buying {quantity} items")
+print(f"Your class has {num_of_students} students")
+
+# Float = floating point numbers , The term "floating" refers to the decimal point's ability to "float"
+price = 18.27
+gpa = 3.2
+distance = 1.6
+
+print(f"The price is ${price}")
+print(f"Your gpa is {gpa}")
+print(f"You ran {distance}km")
+
+# Boolean = Either True or False
+is_student = True
+for_sale  = False
+is_online = True
+
+print(f"Are you a student?: {is_student}")
+
+# We will more likely to see the use of Boolean internally with in a program such as woking with if statemnet
+
+if is_student:
+    print("You are a student")
+else:
+    print("You are not a student")
+
+if for_sale:
+    print("This item is for sale")
+else:
+    print("This item is not available")
+
+if is_online:
+    print("You are online")
+else:
+    print("You are offline")
---
+++ 01_basics/03_type_casting.py
@@ -0,0 +1,60 @@
+# typecasting =  The process of converting a value of one data type to another
+#                (string, integer, float, boolean)
+#                Explicit vs Implicit
+
+# The user input is always a string , so we need to convert it to a type according to our need.
+
+# ------------------ Explicit typecasting -----------------------------
+# When manually converting a value or variable into a different datatype
+
+name = "Ali"
+age = 21
+gpa = 3.2
+student = False
+
+print(type(name))     # type() function tells us the datatype of the variable
+print(type(age))
+print(type(gpa))
+print(type(student))
+
+age = float(age)
+print(type(age))
+print(age)
+
+gpa = int(gpa)
+print(gpa)
+
+student = str(student)
+print(type(student))
+
+# num ---> bool
+age = bool(age)
+print(age)       # when converting a number to boolean if that number is anything but zero it will be true
+
+# Casting somthing to boolean will be useful in this way, if We ask a user to enter a name, I need to check to see
+# if they enter in something, if it's true , it means they have entered something, if false it means, they have not
+# entered anything
+
+name = bool(name)
+print(name)     # This is true , but if this was an empty string it would be false like this name = ""
+                # even the space like this name = " "   registers as true
+
+
+#  ------------------------ Implicit typecasting -------------------------------
+# When a value or variable is converted into a different data type automatically
+
+x = 2
+y = 2.0
+
+x = x / y   # x is implicitly converted to a float during type casting
+
+print(x)
+
+#isinstance(x, int): This checks if x is an integer or a subtype of an integer
+# You can even check against multiple types
+
+price = "hello"
+if isinstance(price, (int, float)):
+    print("Valid numeric value for price.")
+else:
+    print("Invalid numeric type for price.")
