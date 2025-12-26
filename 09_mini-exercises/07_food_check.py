ae794134855da5d3f0f5361875d36639f5a2cb8b
AbdulRehmanSaeed <140309861+AbdulRehman393@users.noreply.github.com>
2025-12-26 22:52:05 +0500

    Add files via upload

---
+++ 09_mini-exercises/07_food_check.py
@@ -0,0 +1,7 @@
+
+response = input("Would you like food? (Y/N): ").upper()     # .upper( ) method will convert to upper alphabet if user enter lower alphabet
+
+if response == "Y":
+    print("Have some food!")
+else:
+    print("No food for you!")
