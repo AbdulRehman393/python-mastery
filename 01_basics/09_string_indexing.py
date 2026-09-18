2d62b2240375e0673ee4e8bf385773e50ef9ded5
AbdulRehmanSaeed <khawajaabdulrehman393@gmail.com>
2026-09-18 17:57:30 +0500

    Add files via upload

--- 01_basics/09_string_indexing.py
+++ 01_basics/09_string_indexing.py
@@ -8,8 +8,8 @@ print(credit_number[0])
 print(credit_number[:4])                    # Python will assume it the starting position will be the beginning of the string
 print(credit_number[5:9])                   # the ending index is exclusive
 print(credit_number[5:])                    # everything besides  4 digits to the end of the string
-print(credit_number[-3])                    # if you need character in a string
-print(credit_number[::2])                   # this will print every second character with in our string
+print(credit_number[-3])                    # if you need character from last in a string that will be third last character
+print(credit_number[::2])                   # this will print every second character within our string
 print(credit_number[2:-1:3])                # now it will count every third character
 
 
